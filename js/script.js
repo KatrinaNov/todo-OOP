9e4e580d41547d707e494058893772ba3c9adca1
katrina <katrina.nov13@gmail.com>
2020-07-03 19:53:19 +0300

    add delete, complete, handler

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 'use strict';
 
 class ToDo {
-  constructor(form, input, todoList, todoCompleted) {
+  constructor(form, input, todoList, todoCompleted, todoContainer) {
     this.form = document.querySelector(form);
     this.input = document.querySelector(input);
     this.todoList = document.querySelector(todoList);
     this.todoCompleted = document.querySelector(todoCompleted);
+    this.todoContainer = document.querySelector(todoContainer);
     this.todoData = new Map(JSON.parse(localStorage.getItem('todoList')));
   }
   addToStorage() {
@@ -17,6 +18,7 @@ class ToDo {
     this.todoData.forEach(this.createItem, this);
     this.addToStorage();
   }
+  // создать новый элемент списка
   createItem(todo) {
     const li = document.createElement('li');
     li.classList.add('todo-item');
@@ -34,6 +36,7 @@ class ToDo {
       this.todoList.append(li);
     }
   }
+  // добавить дело
   addTodo(e) {
     e.preventDefault();
     if (this.input.value.trim()) {
@@ -44,27 +47,45 @@ class ToDo {
       };
       this.todoData.set(newTodo.key, newTodo);
       this.render();
+      this.input.value = '';
+    } else {
+      alert('Введите дело, которое нужно выполнить');
     }
   }
   // генерирует случайный уникальный ключ
   generateKey() {
     return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
   }
-  deleteItem() {
-
+  // удалить
+  deleteItem(key) {
+    this.todoData.delete(key);
+    this.render();
   }
-  completedItem() {
-
+  // поменять статус выполнено/невыполнено
+  completedItem(key) {
+    this.todoData.get(key).completed = !this.todoData.get(key).completed;
+    this.render();
   }
   // обработчики событий
   handler() {
-  // todocontent.addEvent
+    this.todoContainer.addEventListener('click', event => {
+      const target = event.target;
+      const  currentKey = target.closest('li').key;
+      if (target.matches('.todo-complete')) {
+        this.completedItem(currentKey);
+      }
+      if (target.matches('.todo-remove')) {
+        target.closest('li').remove();
+        this.deleteItem(currentKey);
+      }
+    });
   }
   init() {
     this.form.addEventListener('submit', this.addTodo.bind(this));
     this.render();
+    this.handler();
   }
 }
 
-const toDo = new ToDo('.todo-control', '.header-input', '.todo-list', '.todo-completed');
+const toDo = new ToDo('.todo-control', '.header-input', '.todo-list', '.todo-completed', '.todo-container');
 toDo.init();
